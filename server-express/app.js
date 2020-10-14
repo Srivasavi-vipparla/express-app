f839159aea11f951a6ba12d461c8f3a266b27c10
Sri Vasavi <69984398+Srivasavi-vipparla@users.noreply.github.com>
2020-10-14 01:14:11 -0500

    Update app.js

--- server-express/app.js
+++ server-express/app.js
@@ -6,6 +6,13 @@ const app = express()
 app.get('/', (req, res) => {
     res.send(' Hello world ')
   })
+
+app.get('/test', (req, res) => {
+    res.send(' Hello world using test ')
+  })
+app.get('/srivasavi', (req, res) => {
+    res.send(' Hello from srivasavi! ')
+  })
   
   app.listen(3333, () => {
     console.log(`Listening on port 3333!`)
